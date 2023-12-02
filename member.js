7e698b7b25a341f18c8dbe2915eb5040d83c674d
Abraar Sameer <72005838+abraarsameer@users.noreply.github.com>
2023-12-02 11:56:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 25,
+        skills: ["JavaScript", "React", "Angular", "Vue"]
+    };
+    console.log(member.skills[0]);
+    console.log(member.skills[1]);
+    console.log(member.skills[2]);
+    console.log(member.skills[3]);
+} 
